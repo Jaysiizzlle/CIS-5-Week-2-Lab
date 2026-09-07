34724b0277e2469341e7685b1cc4a928c05a2865
Jaysiizzlle <Jaylenchae@gmail.com>
2026-09-07 04:08:41 +0000

    Added My own lines into cout::

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
-// Lab 2 — Your Name
+// Lab 2 — Jaylen Chae 
 // CIS 5 Week 02 · Program structure
 
 int main() {
   // TODO: replace TODOs with YOUR labeled lines (5–6 total)
-  std::cout << "Name: TODO\n";
-  std::cout << "Interest: TODO\n";
-  std::cout << "CIS 5 goal: TODO\n";
-  std::cout << "Tool path: TODO\n";
-  std::cout << "Fun fact: TODO\n";
+  
+  // Using the  for std:: since it's ONLY cout
+  using std::cout; 
+  // My labeled lines
+  cout << "Name: Jaylen Chae\n";
+  cout << "Interest: Photography and Football\n";
+  cout << "CIS 5 goal: Learn C++ and hopefully apply it to learn other coding languages\n";
+  cout << "Tool path: GitHUB Codespace\n";
+  cout << "Fun fact: This is the first time I'm learning how to code\n";
   return 0;
 }
